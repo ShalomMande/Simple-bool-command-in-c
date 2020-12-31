eb90afc95b1d383fd6c88bfb1133d0a7dddcb2f6
shalom mande <shalom.mande@gmail.com>
2020-12-31 01:39:01 -0500

    Add project files.

---
+++ Bool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool shalomisnice = true;
+
+            Console.WriteLine("Is shalom nice");
+            Console.WriteLine(shalomisnice);
+
+        }
+    }
+}
